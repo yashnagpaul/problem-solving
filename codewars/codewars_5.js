2481d8b7029272ffed10481377adfa0b0fc1dcb3
Yash Nagpaul <yash.nagpaul@gmail.com>
2020-11-03 22:51:07 -0800

    adding codewars #5

---
+++ codewars/codewars_5.js
@@ -0,0 +1,29 @@
+// Given an array containing hashes of names,
+// Return a string formatted as a list of names separated by commas
+// except for the last two names, which should be separated by an ampersand.
+
+// EXAMPLES:
+
+// list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// returns 'Bart, Lisa & Maggie'
+
+// list([ {name: 'Bart'}, {name: 'Lisa'} ])
+// returns 'Bart & Lisa'
+
+// list([ {name: 'Bart'} ])
+// returns 'Bart'
+
+// list([])
+// returns ''
+
+function list(names){
+    if (names.length<1) return '';
+    else if (names.lenth == 1) return names[0].name;
+    else {
+      let answer = names[0].name;
+      for(i=1;i<=names.length-1;i++){
+        names[names.length-1].name == names[i].name ? answer = answer.concat(` & ${names[i].name}`) : answer = answer.concat(`, ${names[i].name}`); 
+      }
+      return answer;
+    }
+  }
