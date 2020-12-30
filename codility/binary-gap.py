e14b53384556cac49d452d7ad2b904c9e5e2beac
Yash Nagpaul <yash.nagpaul@gmail.com>
2020-12-30 14:22:32 -0800

    work in progress Codility -> Binary-Gap.py

--- codility/binary-gap.py
+++ codility/binary-gap.py
@@ -2,14 +2,32 @@
 # returns the length of its longest binary gap.
 # The function should return 0 if N doesn't contain a binary gap.
 
-def solution(n: int):
-    binary = bin(n)
-    zero_count = 0
-    biggest_gap = 0
-    for digit in binary:
-        if digit == 0:
-            1
-    return(binary)
+import re
 
 
-solution(1234)
+class Solution:
+    def binaryGap(self, n: int) -> int:
+
+        # binary = str(bin(n))[2:] if (str(bin(n))[1] != "\d") else bin(n)
+
+        binary = bin(n)  # 110 -> 1
+        zero_count = 0
+        one_count = 0
+        biggest_gap = 0
+
+        for digit in binary:
+
+            if (digit == '0'):
+                zero_count += 1
+            else:
+                one_count += 1
+                if biggest_gap < zero_count:
+                    biggest_gap = zero_count+1
+                zero_count = 0  # reset zero count
+                # now see if we have 2 consecutive non-zero values
+                if (one_count > 1) and (biggest_gap < one_count):
+                    biggest_gap = 1
+                # if we've found 2 consecutive ones, reset one count
+                one_count = 0 if one_count > 1 else one_count
+
+        return biggest_gap
